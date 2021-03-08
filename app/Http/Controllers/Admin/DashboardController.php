eb1873a18b53d9d2e178fe79ac360b631c0151bc
AdvenAdam <advenadam@student.uns.com>
2021-03-08 11:12:49 +0700

    menambahkan data di tampilan dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Toko;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('admin.dashboard');
+        $data =[
+        'kategori' =>count( Category::get()),
+        'toko' => count(Toko::get()),
+        'produk' => count(Product::get()),
+        ];
+        return view('admin.dashboard',$data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,28 +11,12 @@
                             <div class="card-icon">
                                 <i class="material-icons">equalizer</i>
                             </div>
-                            <p class="card-category">Jumlah Kategori</p>
-                            <h3 class="card-title">75.521</h3>
+                            <p class="card-category">Jumlah Kategpri</p>
+                            <h3 class="card-title">{{ $kategori }}</h3>
                         </div>
                         <div class="card-footer">
                             <div class="stats">
-                                <i class="material-icons">local_offer</i> Tracked from Google Analytics
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-rose card-header-icon">
-                            <div class="card-icon">
-                                <i class="material-icons">equalizer</i>
-                            </div>
-                            <p class="card-category">Website Visits</p>
-                            <h3 class="card-title">75.521</h3>
-                        </div>
-                        <div class="card-footer">
-                            <div class="stats">
-                                <i class="material-icons">local_offer</i> Tracked from Google Analytics
+                                <i class="material-icons">local_offer</i> UMKM Desa Kemuning
                             </div>
                         </div>
                     </div>
@@ -41,14 +25,14 @@
                     <div class="card card-stats">
                         <div class="card-header card-header-success card-header-icon">
                             <div class="card-icon">
-                                <i class="material-icons">store</i>
+                                <i class="material-icons">production_quantity_limits</i>
                             </div>
-                            <p class="card-category">Revenue</p>
-                            <h3 class="card-title">$34,245</h3>
+                            <p class="card-category">Jumlah Produk</p>
+                            <h3 class="card-title">{{ $produk }}</h3>
                         </div>
                         <div class="card-footer">
                             <div class="stats">
-                                <i class="material-icons">date_range</i> Last 24 Hours
+                                <i class="material-icons">local_offer</i> UMKM Desa Kemuning
                             </div>
                         </div>
                     </div>
@@ -57,18 +41,19 @@
                     <div class="card card-stats">
                         <div class="card-header card-header-info card-header-icon">
                             <div class="card-icon">
-                                <i class="fa fa-twitter"></i>
+                                <i class="material-icons">store</i>
                             </div>
-                            <p class="card-category">Followers</p>
-                            <h3 class="card-title">+245</h3>
+                            <p class="card-category">Jumlah Toko</p>
+                            <h3 class="card-title">{{ $toko }}</h3>
                         </div>
                         <div class="card-footer">
                             <div class="stats">
-                                <i class="material-icons">update</i> Just Updated
+                                <i class="material-icons">local_offer</i> UMKM Desa Kemuning
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
