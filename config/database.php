8552f15e57adb7586ab1f328a9c1457ea75a9919
paul-lestyo <lestyo24@gmail.com>
2021-03-24 00:40:04 +0700

    update config database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$DATABASE_URL = parse_url('postgres://oqkutnhbhozmjw:3e6a5bda6b1f1bed97089516e44ec89a5a40748fabfb47896d7504472c61043e@ec2-52-21-252-142.compute-1.amazonaws.com:5432/d9m3b2h4v1v6h2');
+
 use Illuminate\Support\Str;
 
 return [
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
